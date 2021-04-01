4232cc9c48e482d4caa523db07cff1d33eec8c5d
Sahil Saini <sahilsaini@Sahils-iMac.local>
2021-04-01 18:25:05 -0300

    Board implementation for dual port system

---
+++ enhanced_system_dual_port_board/tcl_scripts/Virtual_IO_Control_DE2_2215.bat
@@ -0,0 +1,15 @@
+@ REM ######################################
+@ REM # Variable to ignore <CR> in DOS
+@ REM # line endings
+@ set SHELLOPTS=igncr
+
+@ REM ######################################
+@ REM # Variable to ignore mixed paths
+@ REM # i.e. G:/$SOPC_KIT_NIOS2/bin
+@ set CYGWIN=nodosfilewarning
+
+@ set QUARTUS_BIN=%QUARTUS_ROOTDIR%\bin
+@ if not exist "%QUARTUS_BIN%" set QUARTUS_BIN=%QUARTUS_ROOTDIR%\bin64
+
+%QUARTUS_ROOTDIR%\sopc_builder\bin\system-console.exe --desktop_script=dashboard.tcl
+
